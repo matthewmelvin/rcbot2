191e5849ad802fad61bf292e1f3279456adb93ae
R. Hunter <robocop@lycos.co.uk>
2025-05-25 16:24:24 +0100

    Linux repair

--- utils/RCBot2_meta/bot_tf2_points.h
+++ utils/RCBot2_meta/bot_tf2_points.h
@@ -426,11 +426,11 @@ public:
 			iszOverlay = NULL_STRING;
 			iPlayersRequired = 0;
 			iTimedPoints = 0;
-			/*for (string_t& i : iszPreviousPoint)
+			// Initialize the raw array using a loop
+			for (string_t& i : iszPreviousPoint)
 			{
 				i = NULL_STRING;
-			}*/
-			std::fill(std::begin(iszPreviousPoint), std::end(iszPreviousPoint), NULL_STRING);
+			}
 			iTeamPoseParam = 0;
 		}
 
